052bdf9c5731e85e12098c1ce6906087098a4b04
SauravSatpute <saurav.satpute5@gmail.com>
2023-12-11 22:34:18 +0530

    My Projects added

---
+++ MyProjects/vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Hello Everyone, This is my first project.</h1>
+    </>
+  )
+}
+
+export default App
---
+++ MyProjects/vite-project/src/Footer.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+export default class Footer extends Component {
+    render() {
+        return <>
+            <h1> This is Footer Component!</h1>
+        </>
+    }
+}
+
